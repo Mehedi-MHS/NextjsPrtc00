667bbbb912ccc34a429c7ef98486b2cba9ca4ce7
Mehedi-MHS <mehedishuvo685638@gmail.com>
2023-06-03 15:34:50 +0600

    First commit of first Hello World App

---
+++ hello_world/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+    <div className='w-1/2 bg-blue-600 rounded grid place-items-center h-32'>
+      <h1 className='text-white text-3xl'>Hello World</h1>
+    </div>
+    </>
+  )
+}
